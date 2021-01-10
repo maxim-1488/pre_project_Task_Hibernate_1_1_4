ddc0ac72c7101de5b6fc77e71f75a73729454ad1
Maxim <m.krylov88@mail.ru>
2021-01-11 01:10:56 +0300

    Выполнено подключение к БД

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,38 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    public static void main(String[] args) throws SQLException,
+            ClassNotFoundException {
+
+        System.out.println("Get connection ... ");
+
+        // Get a Connection object
+        Connection conn = Util.getMySQLConnection();
+
+        System.out.println("Get connection " + conn);
+
+        System.out.println("Done!");
+    }
+
+    // реализация настройки соеденения с БД
+    public static Connection getMySQLConnection() throws SQLException, ClassNotFoundException {
+        String hostName = "localhost";
+        String dbName = "pre_proejct_1";
+        String userName = "root";
+        String password = "14091988";
+
+        return getMySQLConnection(hostName, dbName, userName, password);
+    }
+
+    public static Connection getMySQLConnection(String hostName, String dbName, String userName,
+                                                String password) throws SQLException, ClassNotFoundException {
+        // "?serverTimezone=UTC" - эта запись для установки часового пояса MySQL
+        String connectionURL = "jdbc:mysql://" + hostName + ":3306/" + dbName + "?serverTimezone=UTC";
+        Connection conn = DriverManager.getConnection(connectionURL, userName, password);
+        return conn;
+    }
 }
