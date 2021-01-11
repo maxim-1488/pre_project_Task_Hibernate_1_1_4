a9ab9f5e180736ba4d5cb5ceb0250c51c2ce0c90
Maxim <m.krylov88@mail.ru>
2021-01-12 00:04:23 +0300

    Выполнено подключение к БД

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,38 +1,62 @@
 package jm.task.core.jdbc.util;
 
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Util {
-    public static void main(String[] args) throws SQLException,
-            ClassNotFoundException {
 
-        System.out.println("Get connection ... ");
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    // "?serverTimezone=UTC" - эта запись для установки часового пояса MySQL
+    private static final String URL = "jdbc:mysql://localhost:3306/pre_proejct_1?serverTimezone=UTC";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "14091988";
 
-        // Get a Connection object
-        Connection conn = Util.getMySQLConnection();
+    public static Connection getMySQLConnection() {
 
-        System.out.println("Get connection " + conn);
+        Connection connection = null;
 
-        System.out.println("Done!");
-    }
+        try {
+            Class.forName(DRIVER);
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
 
-    // реализация настройки соеденения с БД
-    public static Connection getMySQLConnection() throws SQLException, ClassNotFoundException {
-        String hostName = "localhost";
-        String dbName = "pre_proejct_1";
-        String userName = "root";
-        String password = "14091988";
-
-        return getMySQLConnection(hostName, dbName, userName, password);
-    }
-
-    public static Connection getMySQLConnection(String hostName, String dbName, String userName,
-                                                String password) throws SQLException, ClassNotFoundException {
-        // "?serverTimezone=UTC" - эта запись для установки часового пояса MySQL
-        String connectionURL = "jdbc:mysql://" + hostName + ":3306/" + dbName + "?serverTimezone=UTC";
-        Connection conn = DriverManager.getConnection(connectionURL, userName, password);
-        return conn;
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return connection;
     }
 }
+
+    //throws SQLException,
+//            ClassNotFoundException {
+//
+//        System.out.println("Get connection ... ");
+//
+//        // Get a Connection object
+//        Connection conn = Util.getMySQLConnection();
+//
+//        System.out.println("Get connection " + conn);
+//
+//        System.out.println("Done!");
+//    }
+
+    // реализация настройки соеденения с БД
+
+//    public static Connection getMySQLConnection() throws SQLException, ClassNotFoundException {
+//        String hostName = "localhost";
+//        String dbName = "pre_proejct_1";
+//        String userName = "root";
+//        String password = "14091988";
+//
+//        return getMySQLConnection(hostName, dbName, userName, password);
+//    }
+//
+//    public static Connection getMySQLConnection(String hostName, String dbName, String userName,
+//                                                String password) throws SQLException, ClassNotFoundException {
+//        // "?serverTimezone=UTC" - эта запись для установки часового пояса MySQL
+//        String connectionURL = "jdbc:mysql://" + hostName + ":3306/" + dbName + "?serverTimezone=UTC";
+//        Connection conn = DriverManager.getConnection(connectionURL, userName, password);
+//        return conn;
+//    }
+
