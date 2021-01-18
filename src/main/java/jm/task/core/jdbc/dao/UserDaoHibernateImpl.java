3b364e9310c41d4b1430b75c8c10dabbe6aafcdf
Maxim <m.krylov88@mail.ru>
2021-01-19 00:03:58 +0300

    Создано подключение к Hibernate

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,42 +1,90 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
+
+    //private final Session session = Util.getSessionFactory().openSession();
+
     public UserDaoHibernateImpl() {
-
     }
-
 
     @Override
     public void createUsersTable() {
+        Session session = Util.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
 
+        Query query = session.createSQLQuery("CREATE TABLE users (id BIGINT NOT NULL AUTO_INCREMENT, " +
+                "name VARCHAR(45), lastName VARCHAR(45), age SMALLINT NOT NULL, " +
+                "PRIMARY KEY (id))").addEntity(User.class);
+
+        transaction.commit();
+        session.close();
     }
 
     @Override
     public void dropUsersTable() {
+        Session session = Util.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
 
+        Query query = session.createSQLQuery("DROP TABLE IF EXISTS users").addEntity(User.class);
+
+        transaction.commit();
+        session.close();
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-
+        try (Session session = Util.getSessionFactory().openSession()) {
+            session.beginTransaction();
+            //session.createSQLQuery("INSERT INTO users (name, lastName, age) VALUES (?, ?, ?)");
+            Query query = session.createQuery("insert into users");
+            query.setParameter(1, name);
+            query.setParameter(2, lastName);
+            query.setParameter(3, age);
+            session.beginTransaction().commit();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+            try {
+                //session.getTransaction().rollback();
+            } catch (Exception exception) {
+                exception.printStackTrace();
+            }
+        }
     }
 
     @Override
     public void removeUserById(long id) {
 
+
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        List<User> list = null;
+        try (Session session = Util.getSessionFactory().openSession()) {
+            session.beginTransaction();
+            Query query = session.createQuery("from users");
+            list = (List<User>) query.list();
+            session.beginTransaction().commit();
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+
+        return list;
     }
 
     @Override
     public void cleanUsersTable() {
+
 
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,9 +1,17 @@
 package jm.task.core.jdbc.util;
 
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class Util {
 
@@ -26,5 +34,47 @@ public class Util {
         }
         return connection;
     }
+
+
+    public static SessionFactory getSessionFactory() {
+
+        SessionFactory sessionFactory = null;
+
+        try {
+            Configuration configuration = new Configuration();
+            //Настройки Hibernate эквивалентны свойствам hibernate.cfg.xml
+            //Создаем объект для настройки конфигурации
+            Properties properties = new Properties();
+            //Environment - Предоставляет доступ к информации о конфигурации, передаваемой в объект Properties
+            properties.put(Environment.DRIVER, DRIVER);
+            properties.put(Environment.URL, URL);
+            properties.put(Environment.USER, USERNAME);
+            properties.put(Environment.PASS, PASSWORD);
+            //для связи Hibernate с БД
+            properties.put(Environment.DIALECT, "org.hibernate.dialect.MySQL8Dialect");
+
+            //SHOW_SQL Включить запись сгенерированного SQL в консоль
+            properties.put(Environment.SHOW_SQL, "true");
+
+            //CURRENT_SESSION_CONTEXT_CLASS Определение контекста для SessionFactory.getCurrentSession()обработки.
+            properties.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+
+            //HBM2DDL_AUTO Автоматический экспорт / обновление схемы с помощью инструмента hbm2ddl.
+            properties.put(Environment.HBM2DDL_AUTO, "create-drop");
+
+            configuration.setProperties(properties);
+            configuration.addAnnotatedClass(User.class);
+
+            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                    .applySettings(configuration.getProperties()).build();
+            sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        return sessionFactory;
+    }
+
 }
 
